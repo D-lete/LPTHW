a742701c6c6d303e225b0967f32587ab2f5599c1
Benjamin A Pillmore <b.a.pillmore@me.com>
2018-11-21 11:05:01 +0100

    More Commits

---
+++ ex_13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+# read the wyss section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second vsariable is:", second)
+print("Your third variable is:", third)
