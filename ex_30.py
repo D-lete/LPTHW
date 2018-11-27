cbdd63aa0ec89c01157a853b7bfaf44da22027ea
Benjamin A Pillmore <b.a.pillmore@me.com>
2018-11-27 09:48:47 +0100

    Exercises 30 & 31

--- ex_30.py
+++ ex_30.py
@@ -5,20 +5,19 @@ trucks = 15
 
 if cars > people:
 	print("We should take the cars.")
-	elif cars < people:
+	if cars < people:
 		print("We should not take the cars.")
-		else:
+	else:
 			print("We can't decide.")
 			
 if trucks > cars:
 	print("That's too many trucks.")
-	elif trucks < cars:
+	if trucks < cars:
 		print("Maybe we could take the trucks.")
-		else:
+	else:
 			print("We still can't decide")
 
 if people > trucks:
 	print("Alright, let's just take the trucks.")
-	else:
-		print("Fine, let's stay home then.")
-			
+else:
+	print("Fine, let's stay home then.")
