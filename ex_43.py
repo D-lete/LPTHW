cb683e52c771e3871328686ff48a43ba9c9e164b
Benjamin A Pillmore <bapillmore@users.noreply.github.com>
2019-04-13 19:02:44 +0200

    Story Text 5

--- ex_43.py
+++ ex_43.py
@@ -92,8 +92,29 @@ class LaserWeaponArmory(Scene):
                 The lock buzzes one last time and thern you hear the sickening melting sound as the mechanism is fused together. You decide to sit there, and finally the Gothons blow up the ship from their ship and you die.
                     """))
             return 'death'
+class TheBridge(Scene):
+    
+    def enter(self):
+        print(dedent("""
+            You burst onto the Bridge andl with the neutron destruct bomb unbder your arm and surprise 5 Gothons who are trying to take control of the ship. Each of them had as even uglier clown costume than the last. They haven't pulled their weapons out yet, as they see the active bomb under your arm and don't want to set it off.
+                """))
+        action = input("> ")
         
-
+        if action == "throw the bomb":
+            print(dedent("""
+                In a panic, youk throw the bomb at the group of Gothons and make a leap for the door. Right as you drop it a Gothon shoot you right in the back killing you. As you die, you see another Gothon frantically try to disarm the bomb. You die knowing that will probably blowup when it goes off.
+                    """))
+            return 'death'
+        elif action == "slowly place the bomb"
+            print(dedent("""
+                You point your blaster at thek bomb under your arms and the Gothons put their hands up and start to sweat. You inch backward to the door, open it anfd then carwefully place the bomb on the floor, pointing your blaster at it. You then jump through the door, punch the 'CLOSE' button and blast the lock so the Gothons can't get out. Now that the bomb is placed, you run to the escape pod to get off this tin can
+                """))
+            return 'escape_pod'
+        else:
+            print('DOES NOT COMPUTE!')
+            return 'the_bridge'
+            
+            
 class EscapePod(Scene):
 
 	def enter(self):
