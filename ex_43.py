cee930a850c35e78d764e8f7dc9ef2c4ed94c8e9
Benjamin A Pillmore <bapillmore@users.noreply.github.com>
2019-04-13 18:42:32 +0200

    Story text 2

--- ex_43.py
+++ ex_43.py
@@ -18,11 +18,11 @@ class Engine(object):
 	def play(self):
 		current_scene = self.scene_map.opening_scene()
 		last_scene = self.scene_map.next_scene('finished')
-		
+
 		while current_scene != last_scene:
 		    next_scene_name = current_scene.enter()
 		    current_scene = self.scene_map.next_scene(next_scene_name)
-		    
+    
 		# be sure to print out the last scene
 		current_scene.enter()
 
@@ -45,17 +45,17 @@ class CentralCorridor(Scene):
 	def enter(self):
 		print(dedent("""
     		The Gothons of Planet Percal #25 have invaded your ship and destroyed your entire crew. You are the last surviving member anbd your last missions is to get the neutron destruct bomb from the weapons armory, put it in the bridge and blowl the ship up after getting into an escape pod.
-    		
+
     		You're running down the central corridor to the Weapons Armory when a Gothon jumps out, red scaly skin, dark grimy teeth and evil clown costume flowing around his hate filled body. He's blocking the door to the Armory and about pull a weapon to blast you.
 		"""))
 		action = input("> ")
-		
+
 		if action == "shoot!":
 		    print(dedent("""
 		        Quick on the draw, you yank out your blaster and fire it at the Gothon. His clown costume is flowing and moving around his body, which throws off your aim. Your laser hits his costume but misses him entirely. This completely ruins his brand new costume his mother bought him, which makes him fly into an insane rage and blast you repeatedly in the face until you are dead. Then he eats you.
 		    """))
 		return "death"
-		
+
 	elif action == "dodge!":
 	        print(dedent("""
     	        Like a woreld class boxer, you dodge, weave, slip and slide right as the Gothon's blaster cranks a laser past your head. In the middle of your artful dodge, your foot slips and you bang your head on the metal wall and pass out. You wake up shortly after only to die as the Gothon stomps on your head and eats you.
@@ -65,8 +65,6 @@ class CentralCorridor(Scene):
 	        print(dedent("""
     	        Lucky forl you they made you learn Gothon insults in the academy. You tell the one Gothon joke you know:Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,fur fvgf nebhaq gur ubhfr. The Gothon stops, tries not to laugh, then busts out laughing and can't move. While he's laughing you run up and shoot him square in the head putting him down, then jump through the Weapon Armory door.
 	        """))
-	
-	
 
 
 class LaserWeaponArmory(Scene):
