98b9adfc400a45bc67dbc072df6cc835a3a9e3f0
Benjamin A Pillmore <bapillmore@users.noreply.github.com>
2019-04-13 19:34:57 +0200

    Tab Fixing 1

--- ex_43.py
+++ ex_43.py
@@ -2,12 +2,13 @@ from sys import exit
 from random import randint
 from textwrap import dedent
 
+
 class Scene(object):
 
-	def enter(self):
-	    print("This scene is not yet configured.")
-	    print("Subclass it and implement enter().")
-		exit(1)
+    def enter(self):
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
+        exit(1)
 
 
 class Engine(object):
