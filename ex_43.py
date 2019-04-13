3939872b61caabb0a17acc4a8d8f355aaa905d33
Benjamin A Pillmore <bapillmore@users.noreply.github.com>
2019-04-13 18:27:32 +0200

    Story Text 1

--- ex_43.py
+++ ex_43.py
@@ -6,29 +6,61 @@ class Scene(object):
 
 	def enter(self):
 	    print("This scene is not yet configured.")
-	    print("Subclass it and implement enter()")
+	    print("Subclass it and implement enter().")
 		exit(1)
 
 
 class Engine(object):
 
 	def __init__(self, scene_map):
-		pass
+		self.scene_map = scene_map
 
 	def play(self):
-		pass
+		current_scene = self.scene_map.opening_scene()
+		last_scene = self.scene_map.next_scene('finished')
+		
+		while current_scene != last_scene:
+		    next_scene_name = current_scene.enter()
+		    current_scene = self.scene_map.next_scene(next_scene_name)
+		    
+		# be sure to print out the last scene
+		current_scene.enter()
 
 
 class Death(Scene):
-
+    quips = [
+        "You died. You kinda suck at this.",
+        "Your mom would be proud, if she were smarter.",
+        "Such a loser.",
+        "I have a small puppy that is better at this.",
+        "You're worse than your dad's jokes."
+        ]
 	def enter(self):
-		pass
+		print(Death.quips[randint(0, len(self.quips)-1)])
+		exit(1)
 
 
 class CentralCorridor(Scene):
 
 	def enter(self):
-		pass
+		print(dedent("""
+    		The Gothons of Planet Percal #25 have invaded your ship and destroyed your entire crew. You are the last surviving member anbd your last missions is to get the neutron destruct bomb from the weapons armory, put it in the bridge and blowl the ship up after getting into an escape pod.
+    		
+    		You're running down the central corridor to the Weapons Armory when a Gothon jumps out, red scaly skin, dark grimy teeth and evil clown costume flowing around his hate filled body. He's blocking the door to the Armory and about pull a weapon to blast you.
+		"""))
+		action = input("> ")
+		
+		if action == "shoot!":
+		    print(dedent("""
+		        Quick on the draw, you yank out your blaster and fire it at the Gothon. His clown costume is flowing and moving around his body, which throws off your aim. Your laser hits his costume but misses him entirely. This completely ruins his brand new costume his mother bought him, which makes him fly into an insane rage and blast you repeatedly in the face until you are dead. Then he eats you.
+		    """))
+		return "death"
+		
+	elif action == "dodge!":
+	    print(dedent("""
+	    
+	    """))
+	
 
 
 class LaserWeaponArmory(Scene):
