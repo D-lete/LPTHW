0bbe8e2cffcdd7da8392e6c1b97a0f86cfb40739
Benjamin A Pillmore <bapillmore@users.noreply.github.com>
2019-04-13 20:12:05 +0200

    Enabled Cheats

--- ex_43.py
+++ ex_43.py
@@ -75,8 +75,9 @@ class LaserWeaponArmory(Scene):
 		    You do a dive roll into the Weapon Armory, crouch and scan the room for more Gothons that might be hiding. It's dead quiet, too quiet. You stand up and run to the far side of the room and find the neutron bomb in its container. There's a keypad lock on the box and you need the code to get the bomb out. If you get the code wrong 10 times then the lock closes forever and you can't get the bomb. The code is 3 digits.
 			"""))
 		code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
-		guess = input("[keypad]> ")
-		guesses = 0
+		#Cheats Enabled
+		guess = input(f"({code} is the code)[keypad]> ")
+		guesses = 1
 
 		while guess != code and guesses < 10:
 			print("BZZZZZEDDDD!")
@@ -85,7 +86,7 @@ class LaserWeaponArmory(Scene):
 
 		if guess == code:
 			print(dedent("""
-				The container clicks open and the seas breaks, letting gas out. You grab the neutron bomb and run as fast as you can to the bridge where you must place it in the right spot.
+				The container clicks open and the seal breaks, letting gas out. You grab the neutron bomb and run as fast as you can to the bridge where you must place it in the right spot.
 				"""))
 			return "the_bridge"
 		else:
