a33057dc1d4cca8a0bd78e2d1da85ea58f1af531
Benjamin A Pillmore <bapillmore@users.noreply.github.com>
2019-04-20 09:54:08 +0200

    Conformed to PEP8

--- ex_44.py
+++ ex_44.py
@@ -1,23 +1,24 @@
 class Parent(object):
-	
-	def override(self):
-		print("PARENT override()")
-		
-	def implicit(self):
-		print("PARENT implicit()")
-		
-	def altered(self):
-		print("PARENT altered()")
-		
+    def override(self):
+        print("PARENT override()")
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+    def altered(self):
+        print("PARENT altered()")
+
+
 class Child(Parent):
-	
-	def override(self):
-		print("CHILD override()")
-	def altered(self):
-		print("CHILD BEFORE PARENT altered()")
-		super(Child, self).altered()
-		print("CHILD, AFTER PARENT altered()")
-		
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        super(Child, self).altered()
+        print("CHILD, AFTER PARENT altered()")
+
+
 dad = Parent()
 son = Child()
 
@@ -29,3 +30,4 @@ son.override()
 
 dad.altered()
 son.altered()
+
